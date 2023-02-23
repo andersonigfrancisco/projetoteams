bb9d6ef492d3de5109782c1ef97b5a4c8b86da43
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-23 15:03:43 +0100

    Definindo o tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
-import {StatusBar} from 'react-native'
+import {StatusBar} from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import theme from '@theme/index'
 
 import{Groups} from '@screens/Groups'
 
 export default function App() {
 
   return (
-    <>
-    <StatusBar 
-      barStyle="light-content" 
-      backgroundColor="transparent" 
-      translucent
-    />
-     <Groups />
-    </>
+
+    <ThemeProvider theme={theme}>
+
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent
+      />
+
+      <Groups />
+      
+    </ThemeProvider>
    
   )
 }
