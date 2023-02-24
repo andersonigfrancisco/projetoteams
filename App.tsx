b84bd203b3a891379d7d6d66dd70e045a68fc5db
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-24 22:52:24 +0100

    componente do Heade

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ export default function App() {
         translucent
       />
 
-      {!fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Groups /> : <Loading />}
 
     </ThemeProvider>
 
