c41531e47b4e4c82c7978908a2f3082bc6a0e8ea
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-25 00:55:31 +0100

    criando component Highlight

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,23 @@
+ import {Container,Title,Subtitle} from './styles'
+
+ type props = {
+  title:string,
+  subtitle:string
+ }
+ 
+ export function Highlight({title,subtitle}:props){
+
+  return(
+    <Container>
+      
+      <Title>
+        {title}
+      </Title>
+
+      <Subtitle>
+        {subtitle}
+      </Subtitle>
+
+    </Container>
+  )
+ }
