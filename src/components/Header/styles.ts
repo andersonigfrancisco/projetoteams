7a6d1c5185d9e1868707e0916ced1f65b86838f3
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-25 00:33:11 +0100

    finalizando o component Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,4 @@
+import {CaretLeft} from 'phosphor-react-native'
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -13,3 +14,12 @@ export const Logo = styled.Image`
   height: 55px;
 
 `;
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`;
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+  size: 32,
+  color: theme.COLORS.WHITE
+}))``;
