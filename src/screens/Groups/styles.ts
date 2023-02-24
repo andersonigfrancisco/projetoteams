cec0fb7c94161a8b4651b865c0daaa261e08ccea
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-24 21:23:56 +0100

    definição de fontes

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components/native';
 
+
 export const Container = styled.View`
 
   flex:1;
   background-color:${({ theme }) => theme.COLORS.GRAY_600};;
-  align-items:center;
-  justify-content:center ;
+  padding:25px;
 `;
-
