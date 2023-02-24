1bcd58075de3f0b0469e1b191589617bafba3484
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-24 22:54:10 +0100

    componente do Heade

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,12 @@
 import {Container} from './styles'
+import { Header } from '@components/Header'
 
 
 export function Groups(){
 
   return(
    <Container>
-     
+     <Header/>
    </Container>
   )
 }
