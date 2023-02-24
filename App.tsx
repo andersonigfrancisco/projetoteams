221552c7ec9292f3ff41c59360574de80d9bd1d1
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-24 21:23:03 +0100

    definição de fontes

--- App.tsx
+++ App.tsx
@@ -1,24 +1,36 @@
-import {StatusBar} from 'react-native';
+import { StatusBar, ActivityIndicator } from 'react-native';
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
 import { ThemeProvider } from 'styled-components/native';
+
 import theme from '@theme/index'
 
-import{Groups} from '@screens/Groups'
+
+import { Groups } from '@screens/Groups'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
 
   return (
 
     <ThemeProvider theme={theme}>
 
-      <StatusBar 
-        barStyle="light-content" 
-        backgroundColor="transparent" 
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
         translucent
       />
 
-      <Groups />
-      
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+
     </ThemeProvider>
-   
+
   )
 }
