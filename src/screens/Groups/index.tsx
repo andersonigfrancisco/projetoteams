567307bf18c53b5684fa3084140a3d547fe4d2cd
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-25 02:08:03 +0100

    Criando Componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
 import { ListEmpty } from '@components/ListEmpty';
+import {Button} from '@components/Button'
 
 
 export function Groups() {
@@ -36,7 +37,10 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
+      />
 
+      <Button 
+        title='Criar nova turma'
       />
 
     </Container>
