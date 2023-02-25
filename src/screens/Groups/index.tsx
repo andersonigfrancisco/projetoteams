587a121254016ffbcdc348f9ba69464f0adf9c63
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-02-25 01:12:03 +0100

    Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,23 @@
 import {Container} from './styles'
 import { Header } from '@components/Header'
 import {Highlight} from '@components/Highlight'
+import {GroupCard} from '@components/GroupCard'
 
 
 export function Groups(){
 
   return(
    <Container>
+
      <Header/>
+
      <Highlight 
       title='Turmas' 
       subtitle='jogue com a sua turma'/>
+
+      <GroupCard title='Nome da Turma'/>
+      <GroupCard title='Nome da Turma'/>
+      
    </Container>
   )
 }
