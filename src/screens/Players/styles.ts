a049f7378414141f60930da6b857d9be5a8a9890
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-03-02 01:25:06 +0100

    Icon dinamico

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -5,3 +5,11 @@ export const Container = styled.View`
   background-color: ${({theme})=>theme.COLORS.GRAY_600};
   padding: 24px;
 `;
+
+export const Form = styled.View`
+  width: 100%;
+  background-color: ${({theme})=>theme.COLORS.GRAY_700};
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 6px;
+`;
