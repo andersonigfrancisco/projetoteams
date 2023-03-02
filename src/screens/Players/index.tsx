0ecbecbba1d4c42604831cd9e052515d464ba4df
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-03-02 02:10:33 +0100

    filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,15 +1,23 @@
-import { Container, Form } from './styles'
+import { useState } from 'react';
+import { FlatList } from 'react-native'
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
-import {Filter} from '@components/Filter'
+import { Filter } from '@components/Filter'
 
 export function Players() {
+
+  const [team, setTeam] = useState('Time A')
+
+  const [players, setPlayers] = useState([])
+
   return (
     <Container>
+
       <Header showBackButton />
 
       <Highlight
@@ -19,18 +27,33 @@ export function Players() {
 
       <Form>
         <Input
-          placeholder='Nomde da pessoa'
+          placeholder='Nome da pessoa'
           autoCorrect={false}
         />
+
         <ButtonIcon
           icon="add"
         />
       </Form>
 
-      <Filter 
-        title="Time A"
-        isActive
-      />
+      <HeaderList>
+
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
 
       <Button
         type='SECONDARY'
