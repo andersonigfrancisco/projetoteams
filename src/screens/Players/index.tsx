a1ea0c9ea8c60d4c0a04e39474ae01b7dada7653
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-03-02 02:25:08 +0100

    Criando Componente PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -5,10 +5,12 @@ import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 import { PlayerCard } from '@components/PlayerCard';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Filter } from '@components/Filter'
 import { Input } from '@components/Input';
+
 
 
 
@@ -68,6 +70,13 @@ export function Players() {
             onRemove={() => {}}
           />
         )}
+
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
+        
       />
 
       <Button
