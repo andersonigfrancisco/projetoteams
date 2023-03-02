a8afbd8d154b6ff6d21b0ec0516bbe65dc87ee65
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-03-02 01:47:00 +0100

    Componente Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -5,6 +5,7 @@ import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
+import {Filter} from '@components/Filter'
 
 export function Players() {
   return (
@@ -25,6 +26,11 @@ export function Players() {
           icon="add"
         />
       </Form>
+
+      <Filter 
+        title="Time A"
+        isActive
+      />
 
       <Button
         type='SECONDARY'
