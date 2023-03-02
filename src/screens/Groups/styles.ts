daf692d932288426c1d77c8f5776c74d68a9c291
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-03-02 15:33:15 +0100

    concluindo navegação

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
+import {SafeAreaView} from 'react-native-safe-area-context'
 
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex:1;
   background-color:${({ theme }) => theme.COLORS.GRAY_600};;
   padding:25px;
