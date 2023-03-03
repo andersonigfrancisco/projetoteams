6cd04c1a99b0c401678028b4891987a802746a44
andersonigfrancisco <andersonfranciscoig@gmail.com>
2023-03-03 15:49:23 +0100

    Adicionado novod jogadores

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,14 +1,15 @@
 import {useState} from 'react';
+import { Alert } from 'react-native';
 import { Container, Content, Icon } from "./styles";
 import {useNavigation} from '@react-navigation/native'
 
 import {groupCreate} from '@storage/group/groupCreate'
+import { AppError } from '../../utils/AppError';
 
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-
 
 export function NewGroup() {
 
@@ -18,10 +19,18 @@ export function NewGroup() {
 
   async function handlenwe (){
     try {
+      if(group.trim().length===0)
+        return Alert.alert("Novo Grupo","Informe o nome da turma")
+
       await groupCreate(group)
+
       navigation.navigate('players',{group})
+
     } catch (error) {
-      console.log(error)
+      if(error instanceof AppError)
+        Alert.alert("Novo Grupo",error.message)
+      else
+      Alert.alert("Novo Grupo","Não foi possivel criar um novo grupo")
     }
     
   }
@@ -41,8 +50,6 @@ export function NewGroup() {
         <Input placeholder="Nome da turma"
           onChangeText={setGroup}
         />
-
-       
 
         <Button 
           style={{marginTop:20}}
